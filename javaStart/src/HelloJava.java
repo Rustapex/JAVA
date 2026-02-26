095966825c02975528f06b638b11373892d2b7f8
MY_PC <sysking1009@naver.com>
2026-02-27 00:27:33 +0900

    0226 공부한 내용들 올려보기

---
+++ javaStart/src/HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava {
+    // HelloJava라는 클래스를 만듦
+
+    // public static void main -> 프로그램의 시작점
+    public static void main(String[] args){
+        System.out.println("Hello Java");
+//        System.out.println 값을 콘솔에 출력하는 기능
+    }
+}
