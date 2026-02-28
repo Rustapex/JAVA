ffc0db479d2f3a7adb84763cf67022e26f8bb368
YS_LABTOP\syski <sysking1009@naver.com>
2026-03-01 01:28:01 +0900

    0301 새벽에 4번 문제 풀기 완료

--- 0227/src/hardQuest/HardQ04.java
+++ 0227/src/hardQuest/HardQ04.java
@@ -1,4 +1,5 @@
 package hardQuest;
+
 import java.util.Scanner;
 
 public class HardQ04 {
@@ -9,36 +10,54 @@ public class HardQ04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("점수를 입력할 학생 수 : ");
-        int stuCnt = sc.nextInt();
+        int stuCnt = Integer.parseInt(sc.nextLine());
 
         System.out.println("이름, 국어, 영어 수학 점수를 입력하세요. ENTER로 구분");
         String[] stuName = new String[stuCnt];
         int[] korScore = new int[stuCnt];
-        int[] EngScore = new int[stuCnt];
+        int[] engScore = new int[stuCnt];
         int[] mathScore = new int[stuCnt];
+        int sumKor = 0, sumEng = 0, sumMath = 0;
 
-        for(int i=0; i< stuCnt; i++){
-            String result;
+        for (int i = 0; i < stuCnt; i++) {
             stuName[i] = sc.nextLine();
-            korScore[i] = sc.nextInt();
-            EngScore[i] = sc.nextInt();
-            mathScore[i] = sc.nextInt();
+            korScore[i] = Integer.parseInt(sc.nextLine());
+            engScore[i] = Integer.parseInt(sc.nextLine());
+            mathScore[i] = Integer.parseInt(sc.nextLine());
 
-            int avgScore = (korScore[i] + EngScore[i] + mathScore[i])/3;
-            if (avgScore >= 90){
+
+            int avgScore = (korScore[i] + engScore[i] + mathScore[i]) / 3;
+            if (avgScore >= 90) {
+                System.out.println(stuName[i] + " : EXCELLENT");
+                System.out.println("개인 평균은 : " + avgScore);
+
+            } else if (avgScore >= 60) {
+                System.out.println("개인 평균은 : " + avgScore);
+            } else {
+                System.out.println(stuName[i] + " : FAIL");
+                System.out.println("개인 평균은 : " + avgScore);
 
             }
-
-            System.out.println(stuName[i] +"의 평균 점수 : " +avgScore);
-
+            System.out.println("국어 : " + korScore[i]);
+            System.out.println("영어 : " + engScore[i]);
+            System.out.println("수학 : " + mathScore[i]);
 
 
         }
+        for (int i = 0; i < korScore.length; i++) {
+            sumKor += korScore[i];
+            sumEng += engScore[i];
+            sumMath += mathScore[i];
+        }
+        double avgKor = (double) sumKor / korScore.length;
+        double avgEng = (double) sumEng / engScore.length;
+        double avgMath = (double) sumMath / mathScore.length;
+        System.out.printf("국어 평균 : %.1f%n" ,avgKor);
+        System.out.printf("영어 평균 : %.1f%n " , avgEng);
+        System.out.printf("수학 평균 : %.1f%n " , avgMath);
+
+        sc.close();
 
 
-
-
-
-
-            }
+    }
 }
