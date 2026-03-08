b3b4b7bca131d0f4a65aa2b40ff24e0279cbef05
YS_LABTOP\syski <sysking1009@naver.com>
2026-03-09 08:33:09 +0900

    백준 문제 하나 푼거

---
+++ solvedAC/src/Class1/Q10757.java
@@ -0,0 +1,11 @@
+package Class1;
+import java.util.Scanner;
+
+public class Q10757 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A = Long.parseLong(sc.nextLine());
+
+
+    }
+}
